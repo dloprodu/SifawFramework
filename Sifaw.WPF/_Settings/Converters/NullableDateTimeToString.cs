173674c0f4559ec22d0552692713507d4b3a1cae
David <dloprodu@gmail.com>
2014-01-09 22:31:58 +0000

    updated

---
+++ Sifaw.WPF/_Settings/Converters/NullableDateTimeToString.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Sifaw.WPF.Converters
+ * 
+ * Diseñador:   David López Rguez
+ * Programador: David López Rguez
+ * 
+ * ===============================================================================================
+ * Historial de versiones:
+ *   - 05/03/2012: Creación de la clase.
+ * ===============================================================================================
+ * Observaciones:
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows;
+
+using Sifaw.Views.Kit;
+
+
+namespace Sifaw.WPF.Converters
+{
+    public class NullableDateTimeToString : IValueConverter
+	{
+		#region IValueConverter Members
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+            if ((value != null) && (((DateTime)value) != DateTime.MinValue)  && (((DateTime)value) != DateTime.MaxValue)) 
+                return ((DateTime)value).ToString("dd/MM/yyyy");
+
+            return string.Empty;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime date = DateTime.MinValue;
+
+            if ((value != null) && !string.IsNullOrEmpty((string)value) && DateTime.TryParse((string)value, out date))
+                return date;
+
+            return null;
+		}
+
+		#endregion
+	}
+}
---
+++ Sifaw.WPF/_Settings/Converters/VisibleIfStringEmptyConverter.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Sifaw.WPF.Converters
+ * 
+ * Diseñador:   David López Rguez
+ * Programador: David López Rguez
+ * 
+ * ===============================================================================================
+ * Historial de versiones:
+ *   - 05/03/2012: Creación de la clase.
+ * ===============================================================================================
+ * Observaciones:
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows;
+
+using Sifaw.Views.Kit;
+
+
+namespace Sifaw.WPF.Converters
+{
+    public class VisibleIfStringEmptyConverter : IValueConverter
+	{
+		#region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value == null || value.ToString().Trim().Length == 0) 
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Don't need to implement this
+            return null;
+        }
+
+		#endregion
+	}
+}

a68d68389205d24e6c6032ec09433bf60ba76076
David <dloprodu@gmail.com>
2014-01-12 19:11:10 +0000

    updated

---
+++ Sifaw.WPF/_Settings/Converters/DecimalToString.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Sifaw.WPF.Converters
+ * 
+ * Diseñador:   David López Rguez
+ * Programador: David López Rguez
+ * 
+ * ===============================================================================================
+ * Historial de versiones:
+ *   - 05/03/2012: Creación de la clase.
+ * ===============================================================================================
+ * Observaciones:
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows;
+
+using Sifaw.Views.Kit;
+
+
+namespace Sifaw.WPF.Converters
+{
+    public class DecimalToString : IValueConverter
+	{
+		#region IValueConverter Members
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+            if (value != null)
+                return ((decimal)value).ToString("#,##0.000");
+
+            return string.Empty;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal result = decimal.Zero;
+
+            if (value != null)
+                decimal.TryParse((string)value, out result);
+
+            return result;
+		}
+
+		#endregion
+	}
+}
